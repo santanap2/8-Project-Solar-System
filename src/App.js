4976a6b3db9604a8701f6c3383b08cfe980bbf41
PedroSantana <phsantana99@gmail.com>
2022-07-26 18:14:41 -0300

    requisito 2 finalizado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        <p>Sistema Solar</p>
+        <SolarSystem />
       </div>
     );
   }
