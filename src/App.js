83d747f98156be4644ac1b9a6e6e6b7af15f1401
PedroSantana <phsantana99@gmail.com>
2022-07-26 18:40:11 -0300

    requisito 4 finalizado

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
 
-const a = 'testando title h2';
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
         <SolarSystem />
-        <Title headline={ a } />
       </div>
     );
   }
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
+    const headline = 'Planetas';
     return (
-      <div data-testid="solar-system" />
+      <div data-testid="solar-system">
+        <Title headline={ headline } />
+      </div>
     );
   }
 }
