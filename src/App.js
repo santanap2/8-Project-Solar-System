a46635597a4b20f9f46eeebf87e13ebf5ea5c00a
PedroSantana <phsantana99@gmail.com>
2022-07-26 21:04:56 -0300

    requisito 9 finalizado

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
+import MissionCard from './components/MissionCard';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <Header />
         <SolarSystem />
         <Missions />
+        <MissionCard />
       </div>
     );
   }
